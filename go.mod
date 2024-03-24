389954b849eaa5757a79e962588c46d3d2a9d745
akhil <akhiljalagams@gmail.com>
2024-03-24 13:57:57 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/akhiljalagam/go-swaywal
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+}
+
+func main() {
+	unsplashAccessKey := os.Getenv("UNSPLASH_ACCESS_KEY")
+	width := os.Getenv("SWAY_WIDTH")
+	height := os.Getenv("SWAY_HEIGHT")
+	topics := os.Getenv("UNSPLASH_TOPICS")
+	unsplashURL := "https://api.unsplash.com/photos/random?w=" + width + "&h=" + height + "&auto=compress&fm=png&client_id=" + unsplashAccessKey + "&topics=" + topics
+	wallpaperPath := "/tmp/swaywallpaper.png"
+	// Fetch and save the wallpaper
+	if err := fetchAndSaveWallpaper(unsplashURL, wallpaperPath); err != nil {
+		log.Fatalf("Failed to fetch and save wallpaper: %v", err)
+	}
+
+	// Set the wallpaper on SwayWM
+	if err := setSwayWallpaper(wallpaperPath); err != nil {
+		log.Fatalf("Failed to set wallpaper on SwayWM: %v", err)
+	}
+}
+
+func fetchAndSaveWallpaper(url, filepath string) error {
+	resp, err := http.Get(url)
+	checkErr(err)
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return io.ErrUnexpectedEOF
+	}
+
+	var result struct {
+		URLs struct {
+			Full string `json:"full"`
+		} `json:"urls"`
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	checkErr(err)
+
+	imageResp, err := http.Get(result.URLs.Full)
+	checkErr(err)
+	defer imageResp.Body.Close()
+
+	file, err := os.Create(filepath)
+	checkErr(err)
+	defer file.Close()
+
+	_, err = io.Copy(file, imageResp.Body)
+	checkErr(err)
+	return err
+}
+
+func setSwayWallpaper(filepath string) error {
+	cmd := exec.Command("/usr/bin/swaymsg", "output", "*", "bg", filepath, "fill")
+	return cmd.Run()
+}
